a4b2101bd2eb9b1085bd048adbc7a2799edbd678
Aliaksandr Pshanichnik <belylis@gmail.com>
2015-08-23 15:06:12 +0300

    Update autoload.php

--- lib/SocialAuther/autoload.php
+++ lib/SocialAuther/autoload.php
@@ -4,5 +4,7 @@ spl_autoload_register('autoload');
 
 function autoload($class)
 {
-    require_once str_replace('SocialAuther/', '', str_replace('\\', '/', $class) . '.php');
+    if (strpos($class, 'SocialAuther') !== FALSE) {
+        require_once str_replace('SocialAuther/', '', str_replace('\\', '/', $class) . '.php');
+    }
 }
